7caecb726bd4cb543d770e106f8781811d833d6d
wei <linkai.wei@cea.fr>
2023-12-19 13:24:55 +0100

    [TCL] add Rc_inject into parameter

--- src/Multiphase/Front_tracking_discontinu/VDF_VEF/Triple_Line_Model_FT_Disc.cpp
+++ src/Multiphase/Front_tracking_discontinu/VDF_VEF/Triple_Line_Model_FT_Disc.cpp
@@ -63,7 +63,7 @@ Triple_Line_Model_FT_Disc::Triple_Line_Model_FT_Disc() :
   num_column_theta_(1),
   num_column_qtcl_(2),
   Rc_tcl_GridN_(4),
-  Rc_inject_(8e-05),
+  Rc_inject_(0.),
   thetaC_tcl_(150.),
   tempC_tcl_(10.),
   integration_time_(0.),
@@ -159,7 +159,7 @@ void Triple_Line_Model_FT_Disc::set_param(Param& p)
   p.ajouter("initial_CL_xcoord", &initial_CL_xcoord_); // XD_ADD_P floattant Initial interface position (unused)
   p.ajouter("read_via_file", &read_via_file_);
   p.ajouter("Rc_tcl_GridN", &Rc_tcl_GridN_); // XD_ADD_P floattant Radius of nucleate site; [in number of grids]
-  // p.ajouter("Rc_inject", &Rc_inject_);
+  p.ajouter("Rc_inject", &Rc_inject_);
   p.ajouter("thetaC_tcl", &thetaC_tcl_); // XD_ADD_P floattant imposed contact angle [in degree] to force bubble pinching / necking once TCL entre nucleate site
   p.ajouter_flag("reinjection_tcl", &reinjection_tcl_); // XD_ADD_P flag This flag activates the automatic injection of a new nucleate seed with a specified shape when the temperature in the nucleation site becomes higher than a certain threshold (tempC_tcl). The shape of the seed is determined by the radius Rc_tcl_GridN and the contact angle thetaC_tcl. The nucleation site is considered free when there are no bubbles present. The site size is defined by Rc_tcl_GridN. This temperature threshold, termed tempC_tcl, is the activation temperature. Setting this temperature implies a wall temperature, therefore, activating reinjection_tcl is ONLY possible for a simulation coupled with solid conduction. NL2 When reinjection_tcl is activated, the values of tempC_tcl (default 10K), Rc_tcl_GridN (default 4 grid sizes), and thetaC_tcl (default 150 degrees) should be provided. Unless (STRONGLY not recommended), the default values (indicated in parentheses) will be used. NL2 If reinjection_tcl is not activated (by default), the mechanism of Numerically forcing bubble pinching/necking will be used for multi-cycle simulation. Once the Triple Contact Line (TCL) enters the nucleation site, a big contact angle thetaC_tcl is imposed to initiate bubble pinching/necking. After the bubble pinching ends, the large bubble above will depart, leaving the remaining part to serve as the nucleate seed. This process is equivalent to immediately inserting a new seed with a prescribed shape (determined by the nucleation site size and contact angle) once a bubble departs. Site size is defined by Rc_tcl_GridN (default 4 grid sizes). Contact angle thetaC_tcl (default 150 degrees). Useful for a standalone (not coupling with solid conduction) simulation.
   p.ajouter_flag("distri_first_facette", &distri_first_facette_); // XD_ADD_P flag This flag determines whether to distribute the Qtcl into all grids occupied by the first facette according to their area proportions. When set, the flux is redistributed into all grids occupied by the first facette based on their area proportions. Default value is 0, the flux is distributed differently: similar to the Meso zone, it is only distributed to grids within the Micro-zone (where the height of the front y is smaller than the size of Micro ym). The distribution of this flux is logarithmically proportional to y between 5.6nm (here interpreted as the value 0 in logarithm) and ym. In practice, in most cases, it will distribute all the flux locally in the first grid.
@@ -308,7 +308,17 @@ void Triple_Line_Model_FT_Disc::completer()
   // ymeso_ -= Objet_U::precision_geom;
   ymeso_ = std::fmax(ymeso_,DMINFLOAT);
 
-  if (reinjection_tcl())
+
+
+  if ((Rc_tcl_GridN_ != 4)and(Rc_inject_>DMINFLOAT))
+    {
+      Cerr << "Check your datafile. Rc_tcl_GridN and Rc_inject should not but used simultaneously." << finl;
+      Cerr << "Rc_tcl_GridN = " << Rc_tcl_GridN_ << finl;
+      Cerr << "Rc_inject = " << Rc_inject_ <<finl;
+      Process::exit();
+    }
+
+  if ((Rc_inject_ == 0.) and reinjection_tcl() )
     {
       const Navier_Stokes_FT_Disc& ns = ref_ns_.valeur();
       const Domaine_Cl_dis_base& zcldis = ns.domaine_Cl_dis();
