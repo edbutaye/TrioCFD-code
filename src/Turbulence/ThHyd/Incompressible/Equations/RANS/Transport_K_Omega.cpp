86816a210f3bde6241c94328360cf0ae8fc5493b
triou <trust@cea.fr>
2025-09-18 18:09:20 +0200

    [trustify] fix transport_equation

--- src/Turbulence/ThHyd/Incompressible/Equations/RANS/Transport_K_Omega.cpp
+++ src/Turbulence/ThHyd/Incompressible/Equations/RANS/Transport_K_Omega.cpp
@@ -36,7 +36,7 @@
 #include <Nom.h>
 
 Implemente_instanciable(Transport_K_Omega, "Transport_K_Omega", Transport_K_Omega_base);
-// XD transport_k_omega eqn_base transport_k_omega -1 The (k-omega) transport equation.
+// XD transport_k_omega transport_equation_deriv transport_k_omega -1 The (k-omega) transport equation.
 
 /*! @brief Imprime le type de l'equation sur un flot de sortie.
  *
