42ddc68b0d7bd6dc443025fa1ba201e06dbaffce
Teo BOUTIN <teo.boutin@cea.fr>
2025-12-09 14:54:06 +0100

    sync includeguard with filename

--- src/Turbulence/ThHyd/Modeles_Turbulence/RANS/Hydr/K_Omega_Eps_constants.h
+++ src/Turbulence/ThHyd/Modeles_Turbulence/RANS/Hydr/K_Omega_Eps_constants.h
@@ -12,15 +12,9 @@
 * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *
 *****************************************************************************/
-//////////////////////////////////////////////////////////////////////////////
-//
-// File:        K_Omega_Constants.h
-// Directory:   $TURBULENCE_ROOT/Specializations/
-//
-//////////////////////////////////////////////////////////////////////////////
 
-#ifndef K_Omega_Constants
-#define K_Omega_Constants
+#ifndef K_Omega_Eps_Constants
+#define K_Omega_Eps_Constants
 
 #include <cmath>
 
